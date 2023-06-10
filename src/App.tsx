0cd3d314aae3c278f4018ebd8873265e42344d60
Debbl <me@aiwan.run>
2023-06-10 12:05:57 +0800

    chore: eslint tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="py-6">
       <div className="flex justify-center">
-        <div className="flex gap-x-2 items-center">
+        <div className="flex items-center gap-x-2">
           <button
             className="btn w-16"
             onClick={() => (isStarting ? handleStop() : handleStart())}
@@ -38,7 +38,7 @@ function App() {
             <select
               onChange={handleSelect}
               defaultValue={16}
-              className="border ml-3"
+              className="ml-3 border"
             >
               <option value={16}>16</option>
               <option value={32}>32</option>
@@ -48,15 +48,15 @@ function App() {
         </div>
       </div>
       <div
-        className={`flex mx-auto ${
+        className={`mx-auto flex ${
           size === 16 ? "w-[32rem]" : size === 32 ? "w-[48rem]" : "w-[64rem]"
-        } flex-wrap items-start mt-6`}
+        } mt-6 flex-wrap items-start`}
       >
         {board.map((b, i) => (
           <div key={i} className="flex">
             <button
               className={`border ${
-                size === 16 ? "w-8 h-8" : size === 32 ? "w-6 h-6" : "w-4 h-4"
+                size === 16 ? "h-8 w-8" : size === 32 ? "h-6 w-6" : "h-4 w-4"
               } ${b === 1 && "bg-black"}`}
               onClick={() => handleClick(i)}
             ></button>
