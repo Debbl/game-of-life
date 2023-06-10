006a258a16ea5aee1aea054d8bbb27cdc4d5e6aa
Debbl <me@aiwan.run>
2023-06-10 10:38:55 +0800

    chore: update initial state

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -4,11 +4,36 @@ import { getAdjoinCount } from "../utils";
 
 const useGame = () => {
   const [size, setSize] = useState<TSize>(16);
-  const [board, setBoard] = useState(
-    Array.from({ length: size * size }).fill(0) as TBoard
-  );
-  const [isStarting, setIsStarting] = useState(false);
+  const [board, setBoard] = useState([
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0,
+  ] as TBoard);
+  const [isStarting, setIsStarting] = useState(true);
   const intervalID = useRef<number>();
+
+  const updateBoard = () => {
+    setBoard((prevBoard) => {
+      const nextBoard = [...prevBoard];
+      prevBoard.forEach((b, i) => {
+        const count = getAdjoinCount(i, prevBoard, size);
+        if (b === 0 && count === 3) {
+          nextBoard[i] = 1;
+        } else if (b === 1 && (count < 2 || count > 3)) {
+          nextBoard[i] = 0;
+        }
+      });
+      return nextBoard;
+    });
+  };
 
   const handleClick = (i: number) => {
     setBoard((prev) => {
@@ -18,21 +43,8 @@ const useGame = () => {
     });
   };
   const handleStart = () => {
-    intervalID.current = setInterval(() => {
-      setIsStarting(true);
-      setBoard((prevBoard) => {
-        const nextBoard = [...prevBoard];
-        prevBoard.forEach((b, i) => {
-          const count = getAdjoinCount(i, prevBoard, size);
-          if (b === 0 && count === 3) {
-            nextBoard[i] = 1;
-          } else if (b === 1 && (count < 2 || count > 3)) {
-            nextBoard[i] = 0;
-          }
-        });
-        return nextBoard;
-      });
-    }, 300);
+    setIsStarting(true);
+    intervalID.current = setInterval(updateBoard, 300);
   };
   const handleStop = () => {
     setIsStarting(false);
@@ -46,6 +58,7 @@ const useGame = () => {
   };
 
   useEffect(() => {
+    isStarting && handleStart();
     return () => {
       handleStop();
     };
